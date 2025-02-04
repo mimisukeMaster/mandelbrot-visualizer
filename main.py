f8b63ba35883b3694ace08988d50029d954fb03c
mimisukeMaster <81568941+mimisukeMaster@users.noreply.github.com>
2025-02-04 14:05:45 +0900

    fix:  拡大時にフリーズするバグを修正

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.colors as mcolors
-from matplotlib.widgets import RectangleSelector
-from threading import Timer
+from concurrent.futures import ProcessPoolExecutor
 
 # 描画範囲
 xmin, xmax, ymin, ymax = -2.0, 1.0, -1.5, 1.5
-debounce_timer = None  # タイマーを管理する変数
 
 def mandelbrot(c, max_iter):
     z = 0
     for n in range(max_iter):
         if abs(z) > 2:
-            return n + 1 - np.log(np.log2(abs(z)))  # スムーズなグラデーションのためにスケーリング
+            return n + 1 - np.log(np.log2(abs(z)))  # スムーズなグラデーションのためのスケーリング
         z = z*z + c
     return max_iter
 
@@ -34,38 +32,41 @@ def create_colormap():
     cmap = mcolors.LinearSegmentedColormap.from_list('custom_cmap', colors, N=256)
     return cmap
 
-def on_range_change(xmin_new, xmax_new, ymin_new, ymax_new):
-    global xmin, xmax, ymin, ymax, ax, img_plot
-
-    # 新しい表示範囲を取得
-    xmin, xmax = xmin_new, xmax_new
-    ymin, ymax = ymin_new, ymax_new
-
-    # マンデルブロ集合を再計算
-    new_img = generate_mandelbrot(xmin, xmax, ymin, ymax, 800, 800, 200)
-
-    # 画像データを更新
-    img_plot.set_array(new_img)
+def update_plot(future, new_bounds):
+    """バックグラウンドスレッドから描画を更新"""
+    global ax, img_plot, fig
+    new_xmin, new_xmax, new_ymin, new_ymax = new_bounds
+    new_img = future.result()
     
-    # 表示範囲を更新
-    img_plot.set_extent((xmin, xmax, ymin, ymax))
-    ax.set_xlim(xmin, xmax)
-    ax.set_ylim(ymin, ymax)
+    # 画像データを更新
+    img_plot.set_data(new_img)
+    img_plot.set_extent((new_xmin, new_xmax, new_ymin, new_ymax))
+    ax.set_xlim(new_xmin, new_xmax)
+    ax.set_ylim(new_ymin, new_ymax)
 
     # 再描画
     fig.canvas.draw_idle()
 
-def delayed_on_range_change(event_ax):
-    global debounce_timer
-    if debounce_timer is not None:
-        debounce_timer.cancel()  # 既存のタイマーをキャンセル
+def on_mouse_release(event, width=800, height=800, max_iter=200):
+    global xmin, xmax, ymin, ymax, executor
+    
+    if event.inaxes != ax:  # グラフ内でのイベントでない場合は無視
+        return
 
-    # 新たにタイマーをセット (500ms後に実行)
-    debounce_timer = Timer(0.5, on_range_change, [event_ax.get_xlim()[0], event_ax.get_xlim()[1], event_ax.get_ylim()[0], event_ax.get_ylim()[1]])
-    debounce_timer.start()
+    # 新しい表示範囲を取得
+    new_xmin, new_xmax = ax.get_xlim()
+    new_ymin, new_ymax = ax.get_ylim()
+
+    # バックグラウンドでマンデルブロ集合を再計算
+    future = executor.submit(generate_mandelbrot, new_xmin, new_xmax, new_ymin, new_ymax, width, height, max_iter)
+    future.add_done_callback(lambda fut: update_plot(fut, (new_xmin, new_xmax, new_ymin, new_ymax)))
+
+    # グローバル変数を更新
+    xmin, xmax = new_xmin, new_xmax
+    ymin, ymax = new_ymin, new_ymax
 
 def main():
-    global fig, ax, img_plot
+    global fig, ax, img_plot, executor
 
     # 計算する粒度 縦横の幅をこの値で分割
     width, height = 800, 800
@@ -78,15 +79,17 @@ def main():
 
     # 描画セットアップ
     fig, ax = plt.subplots(figsize=(10, 10))
-    img_plot = ax.imshow(img, extent=(xmin, xmax, ymin, ymax), cmap=cmap, origin='lower', vmin=1)
+    img_plot = ax.imshow(img, extent=(xmin, xmax, ymin, ymax), cmap=cmap, origin='lower', vmin=1, aspect='auto')
     ax.set_title("Mandelbrot Set")
     ax.set_xlabel("Re")
     ax.set_ylabel("Im")
     fig.colorbar(img_plot, label='Iterations')
 
-    # 範囲変更時のイベントを設定
-    ax.callbacks.connect('xlim_changed', delayed_on_range_change)
-    ax.callbacks.connect('ylim_changed', delayed_on_range_change)
+    # プロセスプールを作成
+    executor = ProcessPoolExecutor(max_workers=4)
+
+    # マウスボタンが離されたときのイベントを設定
+    fig.canvas.mpl_connect('button_release_event', on_mouse_release)
 
     plt.show()
 
