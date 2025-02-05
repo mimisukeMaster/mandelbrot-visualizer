c9980ce29b73a7483f7020cbc46e4f8cd6eeeaa7
mimisukeMaster <81568941+mimisukeMaster@users.noreply.github.com>
2025-02-06 01:43:54 +0900

    refactor: 関数の記述順を修正

--- mandelbrot.py
+++ mandelbrot.py
@@ -8,93 +8,6 @@ import gc
 width, height = 800, 800
 max_iter = 200
 
-# 各プロットを計算
-def mandelbrot(c, max_iter):
-    z = 0
-    for n in range(max_iter):
-        if abs(z) > 2:
-            # 発散の閾値を超えたら速さに応じて平滑化し返す
-            return n + 1 - np.log(np.log2(abs(z)))
-        z = z*z + c
-
-    return max_iter
-
-# 指定範囲でマンデルブロ集合を計算
-def generate_mandelbrot(xmin, xmax, ymin, ymax, width, height, max_iter):
-    
-    # 指定範囲を分割
-    x = np.linspace(xmin, xmax, width)
-    y = np.linspace(ymin, ymax, height)
-
-    # [width] * height 個の空の二次元配列を用意
-    img = np.zeros((height, width))
-    
-    for i in range(height):
-        for j in range(width):
-
-            # 複素数を作成
-            c = complex(x[j], y[i])
-            
-            # プロットを計算
-            iter_count = mandelbrot(c, max_iter)
-            
-            # 発散なら補正付き計算回数、収束なら0を格納
-            img[i, j] = iter_count if iter_count < max_iter else 0
-    
-    return img
-
-# カラーマップ
-def create_colormap():
-    colors = [(0, 0, 1), (0, 1, 1), (0, 1, 0), (1, 1, 0), (1, 0, 0), (1, 0, 1)]
-    
-    # リストから256色の線形カラーマップを生成
-    cmap = mcolors.LinearSegmentedColormap.from_list('custom_cmap', colors, N=256)
-    return cmap
-
-# プロット更新
-def update_plot(future, new_xmin, new_xmax, new_ymin, new_ymax):
-    global ax, img_plot, fig
-
-    # 並列計算結果を取得
-    new_img = future.result()
-    
-    # 新しい画像データを設定
-    img_plot.set_data(new_img)
-    
-    # 画像の表示範囲
-    img_plot.set_extent((new_xmin, new_xmax, new_ymin, new_ymax))
-    
-    #各軸の表示範囲
-    ax.set_xlim(new_xmin, new_xmax)
-    ax.set_ylim(new_ymin, new_ymax)
-
-    # 再描画
-    fig.canvas.draw_idle()
-
-    # メモリリーク回避
-    gc.collect()
-
-# 拡大操作時
-def on_mouse_release(event, width=width, height=height, max_iter=max_iter):
-    global xmin, xmax, ymin, ymax, executor
-    
-    # グラフ内でのイベントでない場合は無視
-    if event.inaxes != ax:  
-        return
-
-    # 新しい表示範囲を取得
-    xmin, xmax = ax.get_xlim()
-    ymin, ymax = ax.get_ylim()
-
-    # 並列で再計算する関数を実行
-    future = executor.submit(generate_mandelbrot, xmin, xmax, ymin, ymax, width, height, max_iter)
-    
-    # 並列の計算が全て完了した時に実行する関数オブジェクトを登録
-    # futureは関数オブジェクトの引数として自動的に代入される
-    future.add_done_callback(execute_callback)
-
-def execute_callback(fut):
-    return update_plot(fut, xmin, xmax, ymin, ymax)
 
 def main():
     global width, height, max_iter, fig, ax, img_plot, executor
@@ -127,6 +40,117 @@ def main():
     fig.canvas.mpl_connect('button_release_event', on_mouse_release)
 
     plt.show()
+
+
+def generate_mandelbrot(xmin, xmax, ymin, ymax, width, height, max_iter):
+    """
+    指定範囲でマンデルブロ集合を計算
+
+    Returns:
+        img: 計算結果の二次元配列
+    """
+    # 指定範囲を分割
+    x = np.linspace(xmin, xmax, width)
+    y = np.linspace(ymin, ymax, height)
+
+    # [width] * height 個の空の二次元配列を用意
+    img = np.zeros((height, width))
+    
+    for i in range(height):
+        for j in range(width):
+
+            # 複素数を作成
+            c = complex(x[j], y[i])
+            
+            # プロットを計算
+            iter_count = mandelbrot(c, max_iter)
+            
+            # 発散なら補正付き計算回数、収束なら0を格納
+            img[i, j] = iter_count if iter_count < max_iter else 0
+    
+    return img
+
+
+def mandelbrot(c, max_iter):
+    """
+    各プロットを計算
+
+    Returns:
+        発散時: 発散の度合いを表す数値
+        収束時: max_iter
+    """
+    z = 0
+    for n in range(max_iter):
+        if abs(z) > 2:
+            # 発散の閾値を超えたら速さに応じて平滑化し返す
+            return n + 1 - np.log(np.log2(abs(z)))
+        z = z*z + c
+
+    return max_iter
+
+
+# カラーマップ
+def create_colormap():
+    colors = [(0, 0, 1), (0, 1, 1), (0, 1, 0), (1, 1, 0), (1, 0, 0), (1, 0, 1)]
+    
+    # リストから256色の線形カラーマップを生成
+    cmap = mcolors.LinearSegmentedColormap.from_list('custom_cmap', colors, N=256)
+    return cmap
+
+# 拡大操作時
+def on_mouse_release(event, width=width, height=height, max_iter=max_iter):
+    """
+    拡大操作時
+    """
+    global xmin, xmax, ymin, ymax, executor
+    
+    # グラフ内でのイベントでない場合は無視
+    if event.inaxes != ax:  
+        return
+
+    # 新しい表示範囲を取得
+    xmin, xmax = ax.get_xlim()
+    ymin, ymax = ax.get_ylim()
+
+    # 並列で再計算する関数を実行
+    future = executor.submit(generate_mandelbrot, xmin, xmax, ymin, ymax, width, height, max_iter)
+    
+    # 並列の計算が全て完了した時に実行する関数オブジェクトを登録
+    # futureは関数オブジェクトの引数として自動的に代入される
+    future.add_done_callback(execute_callback)
+
+def execute_callback(fut):
+    return update_plot(fut, xmin, xmax, ymin, ymax)
+
+# プロット更新
+def update_plot(future, new_xmin, new_xmax, new_ymin, new_ymax):
+    """
+    プロットの更新
+
+    Args:
+        future (Future): 再描画範囲の計算結果
+        他: 新しい画像の範囲
+    """
+    global ax, img_plot, fig
+
+    # 並列計算結果を取得
+    new_img = future.result()
+    
+    # 新しい画像データを設定
+    img_plot.set_data(new_img)
+    
+    # 画像の範囲を設定
+    img_plot.set_extent((new_xmin, new_xmax, new_ymin, new_ymax))
+    
+    # 各軸の範囲を設定
+    ax.set_xlim(new_xmin, new_xmax)
+    ax.set_ylim(new_ymin, new_ymax)
+
+    # 再描画
+    fig.canvas.draw_idle()
+
+    # メモリリーク回避
+    gc.collect()
 
 if __name__ == "__main__":
     main()
